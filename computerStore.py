f1de780b19711d2ac08ccc30ba0de4e957340cd2
Maddie <mirtorabi1234@gmail.com>
2025-07-05 11:17:06 -0400

    added new to the tests

--- computerStore.py
+++ computerStore.py
@@ -1,9 +1,9 @@
 import re
 class ComputerStore:
     def __init__(self, GHz, diskSpace, price):
-        self.GHz = []
-        self.diskSpace = []
-        self.price = []
+        self.GHz = ""
+        self.diskSpace = ""
+        self.price = ""
         self.add_GHz(GHz)
         self.add_diskSpace(diskSpace)
         self.add_price(price)
@@ -12,21 +12,21 @@ class ComputerStore:
     def add_GHz (self, GHz):
         pattern = r"\b[6-9]([0-9]*)(\.[0-9]+)? *(GHz|[Gg]iga[Hh]ertz)"
         if re.match(pattern, str(GHz)):
-            self.GHz.append(GHz)
+            self.GHz + GHz
         else:
             raise ValueError("GHz must be 6 GHz or more")
         
     def add_diskSpace(self, diskSpace):
         pattern = r"\b[5-9]([00]|[0-9]+)(\.[0-9]+)? *(GB|[Gg]igabyte)"
         if re.match(pattern, str(diskSpace)):
-            self.diskSpace.append(diskSpace)
+            self.diskSpace + diskSpace
         """else:
             raise ValueError("Disk space must be greater than 500 GB")"""
 
     def add_price (self, price):
         pattern = r"(^/W)$[0-9]{1, 3}(\.[0-9][0-9])?\b"
         if re.match(pattern, str(price)):
-            self.price.append(price)
+            self.price + price
         """else:
             raise ValueError("Price must be something like $444.34 or $45")"""
         
--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 import re
 GHz = "6.5 GHz"
-pattern = r"\b[6-9]([0-9]*)(\.[0-9]+)? *(GHz|[Gg]iga[Hh]ertz)"
-if re.match(pattern, str(GHz)):
-    print("Valid GHz format")
+diskSpace = "1000 GB"
+price = "1200.00"
+pattern1 = r"\b[6-9]([0-9]*)(\.[0-9]+)? *(GHz|[Gg]iga[Hh]ertz)"
+pattern2 = r"\b[5-9]([00]|[0-9]+)(\.[0-9]+)? *(GB|[Gg]igabyte)"
+pattern3 = r"(^/W)$[0-9]{1, 3}(\.[0-9][0-9])?\b"
+if re.match(pattern1, str(GHz)):
+    if re.match(pattern2, str(diskSpace)):
+        if re.match(pattern3, str(price)):
+            print(f"Computer Specs: {GHz}, {diskSpace}, ${price}")
+        else:
+            raise ValueError("Price must be something like $444.34 or $45")
+    else:
+        raise ValueError("Disk space must be greater than 500 GB")
 else:
     raise ValueError("GHz must be 6 GHz or more")
